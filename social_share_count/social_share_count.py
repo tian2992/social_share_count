ddd804febb9ff22f31ec85cd0f2b40462646c51f
Sebastian Oliva <code@sebastianoliva.com>
2018-06-27 14:45:45 -0600

    base app

--- social_share_count/social_share_count.py
+++ social_share_count/social_share_count.py
@@ -1,3 +1,62 @@
 # -*- coding: utf-8 -*-
+import os
+import flask
+from flask import request
+from flask import jsonify
+from urllib.parse import quote
+import twitter
 
 """Main module."""
+
+# import configparser
+# config = configparser.ConfigParser()
+# # User level config file.
+# read_files = config.read(os.path.expanduser('~/.config/ocdsdata/config.ini'))
+# CONSUMER_KEY = config.get('TW_CONSUMER_KEY', 'HOSTNAME')
+
+CONSUMER_KEY = os.environ.get('TW_CONSUMER_KEY', '')
+CONSUMER_SECRET = os.environ.get('TW_CONSUMER_SECRET', '')
+ACCESS_TOKEN_KEY = os.environ.get('TW_ACCESS_TOKEN_KEY', '')
+ACCESS_TOKEN_SECRET = os.environ.get('TW_ACCESS_TOKEN_SECRET', '')
+BASEURL = os.environ.get('BASEURL', '')
+
+tw_api = twitter.Api(consumer_key=CONSUMER_KEY,
+                      consumer_secret=CONSUMER_SECRET,
+                      access_token_key=ACCESS_TOKEN_KEY,
+                      access_token_secret=ACCESS_TOKEN_SECRET)
+tw_api.VerifyCredentials()
+
+
+app = flask.Flask(__name__)
+
+def format_url(url):
+    triml = url.lstrip("https://")
+    trimr = triml.rstrip("/")
+    if trimr.startswith(BASEURL):
+        return trimr
+    return ""
+
+
+def get_twitter_shares(uri):
+    query = "count=100&q={}".format(quote(uri, safe=''))
+    results = tw_api.GetSearch(raw_query=query)
+    return len(results)
+
+
+@app.route('/api/tw/urls/count.json')
+def fetch_twitter_shares():
+    url_query = request.args['url']
+    url_clean = format_url(url_query)
+    try:
+        shares = get_twitter_shares(url_clean)
+    except:
+        shares = -1
+    resp = {"url":url_clean, "count": shares}
+    return jsonify(resp)
+
+
+@app.after_request
+def add_header(response):
+    response.cache_control.public = True
+    response.cache_control.max_age = 3600
+    return response
