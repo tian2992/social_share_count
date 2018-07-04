5c5bc1883f38f01f749d16270c53f57243cf91ce
Sebastian Oliva <code@sebastianoliva.com>
2018-07-04 15:54:53 -0600

    MD

--- social_share_count/database.py
+++ social_share_count/database.py
@@ -1,5 +1,5 @@
 import os
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -12,9 +12,12 @@ except:
     pass
 
 engine = create_engine(DATABASE_URL, convert_unicode=True)
+metadata = MetaData()
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
+metadata.create_all(engine)
+
 Base = declarative_base()
 Base.query = db_session.query_property()
 
